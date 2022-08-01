1b22f29b7ca20b71ec9a2c593cae05c928f61852
Olivia <ramsfol@gmail.com>
2022-08-01 15:06:37 -0500

    Engineer passes all tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+//imports
+const Engineer = require('../lib/Engineer')
+
+//property test
+test("gets engineer's GitHub username", () => {
+    const engineer = new Engineer('fakeGitHub')
+
+    expect(engineer.github).toEqual(expect.any(String))
+})
+
+//function tests
+test("gets engineer's GitHub via getGitHub function", () => {
+    const engineer = new Engineer('fakeGitHub')
+
+    expect(engineer.getGitHub()).toEqual(expect.any(String))
+})
+
+test("gets engineer's role via getRole function", () => {
+    const role = "Engineer"
+    const engineer = new Engineer(role)
+
+    expect(engineer.getRole()).toBe(role)
+})
