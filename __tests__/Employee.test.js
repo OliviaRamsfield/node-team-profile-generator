d31e61a867989a2e3eb32a4934e7c2ca8201c9a0
Olivia <ramsfol@gmail.com>
2022-08-01 14:27:51 -0500

    Employee Class passes all tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,10 @@
 //imports
 const Employee = require('../lib/Employee')
 
+//object and properties tests
 test('creates employee parent object', () => {
     const employee = new Employee()
     expect(typeof(employee)).toBe("object")
-//NEED TO WRTIE TESTS FOR THREE BELOW STILL
-    // expect(employee.name).toBe('Guy')
-    // expect(employee.id).toEqual(expect.any(Number))
-    // expect(employee.email).toEqual(expect.any(String))
 }) 
 
 test("gets employee's name", () => {
@@ -17,14 +14,40 @@ test("gets employee's name", () => {
 })
 
 test("gets employee's id number", () => {
+    const employee = new Employee('Guy', 123)
+
+    expect(employee.id).toEqual(expect.any(Number))
+})
+
+test("gets employee's email", () => {
     const employee = new Employee('Guy', 123, 'email@email.com')
+
+    expect(employee.email).toEqual(expect.any(String))
+})
+
+//function tests
+test("gets employee's id number via getId function", () => {
+    const employee = new Employee('Guy', 123)
 
     expect(employee.getId()).toEqual(expect.any(Number))
 })
 
-test("gets employee name via getName", () => {
+test("gets employee's name via getName function", () => {
     const name = "Guy"
     const employee = new Employee(name)
 
     expect(employee.getName()).toBe(name)
+})
+
+test("gets employee's email via getEmail function", () => {
+    const employee = new Employee('Guy', 123, 'email@email.com')
+
+    expect(employee.getEmail()).toEqual(expect.any(String))
+})
+
+test("gets employee's role via get Role function", () => {
+    const role = "Employee"
+    const employee = new Employee(role)
+
+    expect(employee.getRole()).toBe(role)
 })
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,8 @@
+//imports
+const Employee = require('../lib/Employee')
+const Manager = require('../lib/Manager')
+
+//properties test
+test("gets manager's office number", () => {
+    const manager = new Manager()
+}) 
